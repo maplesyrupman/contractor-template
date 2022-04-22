b2aa5c81e33985167e63d793dba926ec129a072b
maplesyrupman <maplesyrupman@protonmail.com>
2022-04-21 21:32:04 -0400

    contact page, need to configure form

--- pages/contact.js
+++ pages/contact.js
@@ -1,50 +1,59 @@
 import Layout from '../components/Layout'
 import styles from '../styles/contact.module.css'
 
+import Button from '../components/Button'
+
 export default function Contact() {
     return (
         <Layout>
-            <div>
-                <h1 className={styles.alex}>
+            <div className={styles.headingContainer}>
+                <h1 className={`pageHeading blue`}>
                     Contact Us
                 </h1>
             </div>
-            <div>
-                <div>
-                    <h2>Get in touch.</h2>
-                    <div>
-                        <h3>Domenic Tedesco - President</h3>
-                        <p>Email: dom@tedescogroup.ca</p>
-                    </div>
-                    <div>
-                        <h3>Eleanor Lastname - Secretary</h3>
-                        <p>Phone: 416-123-4567</p>
-                        <p>Email: admin@tedescogroup.ca</p>
-                    </div>
-                    <div>
-                        <p>123 Someplace Blvd. Toronto,
-                            ON Canada M1G 2L9
-                        </p>
+            <div className={styles.pageContainer}>
+                <div className={styles.infoContainer}>
+                    <h2 className={styles.headerSecondary}>Get in touch.</h2>
+                    <div className={styles.infoTextContainer}>
+                        <div>
+                            <h3 className={styles.heading}>Domenic Tedesco - President</h3>
+                            <p className={styles.text}>Email: dom@tedescogroup.ca</p>
+                        </div>
+                        <div>
+                            <h3 className={styles.heading}>Eleanor Lastname - Secretary</h3>
+                            <p className={styles.text}>Phone: 416-123-4567</p>
+                            <p className={styles.text}>Email: admin@tedescogroup.ca</p>
+                        </div>
+                        <div>
+                            <h3 className={styles.heading}>Address</h3>
+                            <p className={styles.text}>123 Someplace Blvd. Toronto,
+                                <br />
+                                ON Canada M1G 2L9
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <form>
-                        <h2>Get a quote.</h2>
-                        <div>
-                            <label htmlFor='name'>Name</label>
-                            <input name='name' type='text' />
+                <div className={styles.formContainer}>
+                    <form className={styles.formBody}>
+                        <h2 className={styles.headerSecondary}>Get a quote.</h2>
+                        <div className={styles.fieldContainer}>
+                            <label htmlFor='name' className={styles.label}>Name</label>
+                            <input name='name' type='text' className={styles.fieldInput}/>
+                        </div>
+                        <div className={styles.fieldContainer}>
+                            <label htmlFor='email' className={styles.label}>Email</label>
+                            <input name='email' type='email' className={styles.fieldInput} />
+                        </div>
+                        <div className={styles.fieldContainer}>
+                            <label htmlFor='phone' className={styles.label}>Phone</label>
+                            <input name='phone' type='text'  className={styles.fieldInput}/>
+                        </div>
+                        <div className={styles.fieldContainer}>
+                            <label htmlFor='message' className={styles.label}>Message</label>
+                            <textarea name='message'  className={styles.fieldInput}/>
                         </div>
                         <div>
-                            <label htmlFor='email'>Email</label>
-                            <input name='email' type='email' />
-                        </div>
-                        <div>
-                            <label htmlFor='phone'>Phone</label>
-                            <input name='phone' type='text' />
-                        </div>
-                        <div>
-                            <label htmlFor='message'>Message</label>
-                            <textarea name='message' />
+                            <Button colour='primary' size='small' content={'Submit'} type='submit'/>
                         </div>
                     </form>
                 </div>
