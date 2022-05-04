3d494f8fd983b5118f7f0d927543b45ae45c8352
maplesyrupman <maplesyrupman@protonmail.com>
2022-05-04 16:56:21 -0400

    styling edits

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,14 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-      <link rel='shortcut icon' href='/favicon.ico' />
-    </Head>
-    <Component {...pageProps} />
+      <Head>
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link rel="stylesheet" href="https://use.typekit.net/wdb3lae.css"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
     </>
   )
 }
